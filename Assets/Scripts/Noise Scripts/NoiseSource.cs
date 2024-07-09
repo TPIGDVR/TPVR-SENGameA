3a7569edc1283ef4a02ce431e5d4f4b2b754d974
Lim Yan Chun <105861890+yanchn-lim@users.noreply.github.com>
2024-07-09 09:44:27 +0800

    p

--- Assets/Scripts/Noise Scripts/NoiseSource.cs
+++ Assets/Scripts/Noise Scripts/NoiseSource.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class NoiseSource : MonoBehaviour
 {
+    [SerializeField] Transform noiseIndicator;
     public float NoiseValue = 10 ;
+
+    private void Start()
+    {
+        
+        noiseIndicator.localScale = new Vector3(NoiseValue, 0, NoiseValue) / transform.lossyScale.x;
+    }
 }
