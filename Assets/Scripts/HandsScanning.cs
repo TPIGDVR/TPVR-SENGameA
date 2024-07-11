d635363b92eaad679ba511ad15a05445a8ee5d5c
Ryan Yak <ryanyakkie@gmail.com>
2024-07-11 10:39:17 +0800

    gfdfgtyhujhgfcdxzscdxvfbgnhj,khsfg

--- Assets/Scripts/HandsScanning.cs
+++ Assets/Scripts/HandsScanning.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.ProBuilder;
+using UnityEngine.UI;
 
 public class HandsScanning : MonoBehaviour
 {
 
-    public void Scanning()
+    [SerializeField]
+    private Image scanUI;
+    [SerializeField]
+    private GameObject canvas;
+
+
+
+    private bool scanCompleted;
+    private bool scanStarted = false;
+
+    public bool ScanCompleted {  get { return scanCompleted; } }
+
+
+    private void Update()
     {
-        Debug.Log("WOOF WOOF WORK");
+        if (scanStarted == true)
+        {
+
+        }
+    }
+
+    public void ScanStart()
+    {
+        canvas.SetActive(true);
+        scanStarted = true;
+        
+    }
+
+
+    public void ScanStop()
+    {
+        scanStarted = false;
     }
 
 
 
 }
+
 
