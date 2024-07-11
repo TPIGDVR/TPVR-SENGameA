240c7bc81201f904bae63074fade02d1ec6fde19
Ryan Yak <ryanyakkie@gmail.com>
2024-07-11 11:21:12 +0800

    wertyuim nbvcer56t7yuijkm nbvc

--- Assets/Scripts/HandsScanning.cs
+++ Assets/Scripts/HandsScanning.cs
@@ -13,20 +13,42 @@ public class HandsScanning : MonoBehaviour
     private Image scanUI;
     [SerializeField]
     private GameObject canvas;
+    [SerializeField]
+    private float timeMultiplier;
 
 
 
     private bool scanCompleted;
-    private bool scanStarted = false;
+    public bool scanStarted = false;
 
     public bool ScanCompleted {  get { return scanCompleted; } }
+
+    private void Start()
+    {
+        scanUI.fillAmount = 0f;
+        
+    }
 
 
     private void Update()
     {
+        if (scanUI.fillAmount == 1)
+        {
+            scanCompleted = true;
+        }
+        else if (scanUI.fillAmount == 0)
+        {
+            canvas.SetActive(false);
+        }
+        
+
         if (scanStarted == true)
         {
-
+            scanUI.fillAmount += Time.fixedDeltaTime / 100;
+        }
+        else if (scanStarted != true)
+        {
+            scanUI.fillAmount -= Time.fixedDeltaTime / 100;
         }
     }
 
