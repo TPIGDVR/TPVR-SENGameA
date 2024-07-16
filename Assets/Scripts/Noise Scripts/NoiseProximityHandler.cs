618598cfc61a444971cec42029dfa4b45d2b3ef2
Lim Yan Chun <105861890+yanchn-lim@users.noreply.github.com>
2024-07-16 15:32:39 +0800

    nosie rewroked

--- Assets/Scripts/Noise Scripts/NoiseProximityHandler.cs
+++ Assets/Scripts/Noise Scripts/NoiseProximityHandler.cs
@@ -7,52 +7,18 @@ using UnityEngine.Rendering;
 public class NoiseProximityHandler : MonoBehaviour
 {
     [SerializeField]NoiseSource[] _noiseSources;
-    [SerializeField]
-    float _maxDistanceFromSource;
+
     [SerializeField] float _totalNoiseValue;
-    float _maxSqrDist;
 
-    //[Header("Experimental Ignore")]
-    //[SerializeField]
-    //int numberOfSample = 64;
-    //[SerializeField]
-    //float[] _dataSample;
-
-    //[SerializeField]
-    //float sampleSize;
-    //List<float> avgNoise; 
     private void Start()
     {
-        _noiseSources = FindObjectsOfType<NoiseSource>();
-        _maxSqrDist = _maxDistanceFromSource * _maxDistanceFromSource;
-        
-        
-        //_dataSample = new float[numberOfSample];
-
-        
+        _noiseSources = FindObjectsOfType<NoiseSource>();        
     }
 
     private void Update()
     {
         _totalNoiseValue = CalculateNoiseLevelBasedOffDistance();
 
-        //Ignore this code (only use if it looks cool :))
-        //_totalNoiseValue = CalculateNoiseBasedOnListener();
-
-        //if (avgNoise.Count >= sampleSize)
-        //{
-        //    avgNoise.RemoveAt(0);
-        //}
-
-        //float noise = 0f;
-        //avgNoise.Add(TotalNoiseValue);
-        //for(int i = 0; i < avgNoise.Count; i++)
-        //{
-        //    noise += avgNoise[i];
-        //}
-        //noise /= avgNoise.Count;
-
-        //print($"avg noise {noise}");
     }
 
     float CalculateNoiseLevelBasedOffDistance()
@@ -60,33 +26,16 @@ public class NoiseProximityHandler : MonoBehaviour
         float totalNoiseLevel = 0;
         foreach (var source in _noiseSources)
         {
-            float sqrDistance = Mathf.Abs(Vector3.SqrMagnitude(source.transform.position - Camera.main.transform.position));
+            float dist = Vector3.Distance(Camera.main.transform.position,source.transform.position);
+            if (dist > source.NoiseRangeScaled)
+                continue;
 
-            //skip if out of distance
-            //if (sqrDistance > _maxSqrDist) continue;
-
-            //lerp to its noise value based off distance;
-            float noiseLevel = Mathf.Lerp(source.NoiseValue, 0,sqrDistance / _maxSqrDist);
-            totalNoiseLevel += noiseLevel;
-            
+            float noiseLevel = Mathf.Lerp(source.NoiseValue, 0,dist/source.NoiseRangeScaled);
+            totalNoiseLevel += noiseLevel;          
         }
-        //Debug.Log(totalNoiseLevel);
+
         return totalNoiseLevel;
     }
-
-
-    //float CalculateNoiseBasedOnListener()
-    //{
-    //    AudioListener.GetOutputData(_dataSample,0);
-    //    float sum = 0;
-    //    for (int i = 0; i < _dataSample.Length; i++)
-    //    {
-    //        sum += Mathf.Pow(_dataSample[i], 2);//Mathf.Abs(dataContainer[i]);
-    //    }
-        
-    //    return Mathf.Sqrt(sum / numberOfSample) ;
-    //}
-
 
     public float TotalNoiseValue { get => _totalNoiseValue; }
 }
