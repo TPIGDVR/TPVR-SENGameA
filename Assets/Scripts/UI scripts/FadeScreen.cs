f750d6eac824b18d69b038774ad72bf41ba63dc6
Thom Low <thomthomlow@gmail.com>
2024-07-18 16:49:13 +0800

    safe

--- Assets/Scripts/UI scripts/FadeScreen.cs
+++ Assets/Scripts/UI scripts/FadeScreen.cs
@@ -1,4 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -14,8 +13,7 @@ public class FadeScreen : MonoBehaviour
     private Renderer rend;
     
 
-    [SerializeField]
-    Image fadeOverlay;
+
 
     // Start is called before the first frame update
     void Start()
