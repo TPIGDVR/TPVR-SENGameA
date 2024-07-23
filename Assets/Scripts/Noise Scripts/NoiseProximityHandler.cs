a6c7ee1ee66ae779ba3f9bb3920b8d3d6f6121fb
Lim Yan Chun <105861890+yanchn-lim@users.noreply.github.com>
2024-07-23 17:14:32 +0800

    sound no affect through wall + muffled sound

--- Assets/Scripts/Noise Scripts/NoiseProximityHandler.cs
+++ Assets/Scripts/Noise Scripts/NoiseProximityHandler.cs
@@ -24,10 +24,15 @@ public class NoiseProximityHandler : MonoBehaviour
     float CalculateNoiseLevelBasedOffDistance()
     {
         float totalNoiseLevel = 0;
+        Transform camTrans = Camera.main.transform;
         foreach (var source in _noiseSources)
         {
-            float dist = Vector3.Distance(Camera.main.transform.position,source.transform.position);
-            if (dist > source.NoiseRangeScaled)
+            float dist = Vector3.Distance(camTrans.position,source.transform.position);
+
+            //if (dist > source.NoiseRangeScaled || source.CheckIfBlocked())
+            //    continue;
+
+            if (source.CheckIfBlockedOrOutOfRange())
                 continue;
 
             float noiseLevel = Mathf.Lerp(source.NoiseValue, 0,dist/source.NoiseRangeScaled);
