80de1a987a51029597f10a2fe7e70da7a2d466ef
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-07-26 05:10:09 +0800

    refactor done?

--- Assets/Scripts/UI scripts/ActivateUIOnTap.cs
+++ Assets/Scripts/UI scripts/ActivateUIOnTap.cs
@@ -1,88 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
 
-public class ActivateUIOnTap : MonoBehaviour
-{
-    public TextMeshProUGUI timeText;
-    public TapInteractorCheck TapInteractorCheck;
-    public GameObject[] segments; // Reference to your UI images.
-    private int tapCounter = 0;
+//public class ActivateUIOnTap : MonoBehaviour
+//{
+//    public TextMeshProUGUI timeText;
+//    public TapInteractorCheck TapInteractorCheck;
+//    public GameObject[] segments; // Reference to your UI images.
+//    private int tapCounter = 0;
 
-    private void Start()
-    {
-        DeactivateAllSegments();
-    }
+//    private void Start()
+//    {
+//        DeactivateAllSegments();
+//    }
 
-    void Update()
-    {
-        timeText.text = "Time: " + TapInteractorCheck.tapManager.tapTimer.ToString("F2");
+//    void Update()
+//    {
+//        timeText.text = "Time: " + TapInteractorCheck.tapManager.tapTimer.ToString("F2");
 
-        // Check if the tap counter has increased.
-        if (tapCounter < TapInteractorCheck.tapManager.tapCounter)
-        {
-            // Check if the maximum number of taps has been reached.
-            if (tapCounter >= segments.Length)
-            {
-                // Deactivate all images before resetting tapCounter.
-                DeactivateAllSegments();
-                tapCounter = 0;
-                TapInteractorCheck.tapManager.tapCounter = 0;
-            }
+//        // Check if the tap counter has increased.
+//        if (tapCounter < TapInteractorCheck.tapManager.tapCounter)
+//        {
+//            // Check if the maximum number of taps has been reached.
+//            if (tapCounter >= segments.Length)
+//            {
+//                // Deactivate all images before resetting tapCounter.
+//                DeactivateAllSegments();
+//                tapCounter = 0;
+//                TapInteractorCheck.tapManager.tapCounter = 0;
+//            }
 
-            // Activate the next image from left to right.
-            if (tapCounter < segments.Length)
-            {
-                segments[tapCounter].SetActive(true);
-                tapCounter++;
-            }
-        }
+//            // Activate the next image from left to right.
+//            if (tapCounter < segments.Length)
+//            {
+//                segments[tapCounter].SetActive(true);
+//                tapCounter++;
+//            }
+//        }
 
-        // Check if the tap counter matches the number of segments, and deactivate them in this case.
-        if (tapCounter >= segments.Length)
-        {
-            DeactivateAllSegments();
-            tapCounter = 0;
-            TapInteractorCheck.tapManager.tapCounter = 0;
-        }
+//        // Check if the tap counter matches the number of segments, and deactivate them in this case.
+//        if (tapCounter >= segments.Length)
+//        {
+//            DeactivateAllSegments();
+//            tapCounter = 0;
+//            TapInteractorCheck.tapManager.tapCounter = 0;
+//        }
 
-        // If a tap occurs, but it doesn't maintain the rhythm, reset the tapCounter and deactivate all images.
-        if (TapInteractorCheck.tapManager.tapCounter > 0 && !IsRhythmMaintained())
-        {
-            ResetTaps();
-            StartCoroutine(ChangeColorCoroutine());
-        }
-    }
+//        // If a tap occurs, but it doesn't maintain the rhythm, reset the tapCounter and deactivate all images.
+//        if (TapInteractorCheck.tapManager.tapCounter > 0 && !IsRhythmMaintained())
+//        {
+//            ResetTaps();
+//            StartCoroutine(ChangeColorCoroutine());
+//        }
+//    }
 
-    private void DeactivateAllSegments()
-    {
-        foreach (var segment in segments)
-        {
-            segment.SetActive(false);
-        }
-    }
+//    private void DeactivateAllSegments()
+//    {
+//        foreach (var segment in segments)
+//        {
+//            segment.SetActive(false);
+//        }
+//    }
 
-    private bool IsRhythmMaintained()
-    {
-        // Add your rhythm logic here. For example, you can check the time between the last two taps.
-        // Return true if the rhythm is maintained; otherwise, return false.
-        return TapInteractorCheck.IsRhythmMaintained();
-    }
+//    private bool IsRhythmMaintained()
+//    {
+//        // Add your rhythm logic here. For example, you can check the time between the last two taps.
+//        // Return true if the rhythm is maintained; otherwise, return false.
+//        return TapInteractorCheck.IsRhythmMaintained();
+//    }
 
-    public void ResetTaps()
-    {
-        tapCounter = 0;
-        TapInteractorCheck.tapManager.tapCounter = 0;
-        TapInteractorCheck.tapManager.tapTimer = 0;
-        DeactivateAllSegments();
-    }
+//    public void ResetTaps()
+//    {
+//        tapCounter = 0;
+//        TapInteractorCheck.tapManager.tapCounter = 0;
+//        TapInteractorCheck.tapManager.tapTimer = 0;
+//        DeactivateAllSegments();
+//    }
 
-    IEnumerator ChangeColorCoroutine()
-    {
-        TapInteractorCheck.handMat.color = Color.red;
-        // Wait for a brief moment (adjust the faintDuration as needed)
-        yield return new WaitForSeconds(0.1f);
-        TapInteractorCheck.handMat.color = TapInteractorCheck.handColor;
-    }
-}
+//    IEnumerator ChangeColorCoroutine()
+//    {
+//        TapInteractorCheck.handMat.color = Color.red;
+//        // Wait for a brief moment (adjust the faintDuration as needed)
+//        yield return new WaitForSeconds(0.1f);
+//        TapInteractorCheck.handMat.color = TapInteractorCheck.handColor;
+//    }
+//}
--- Assets/Scripts/UI scripts/BreathCounter.cs
+++ Assets/Scripts/UI scripts/BreathCounter.cs
@@ -1,197 +1,197 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
-using UI;
-using Unity.VisualScripting;
-using UnityEditor;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.InputSystem;
+//using UnityEngine.XR.Interaction.Toolkit;
+//using UI;
+//using Unity.VisualScripting;
+//using UnityEditor;
 
-public class BreathCounter : UIBase
-{
-    [Header("breath counter")]
-    public Scrollbar progressBar;
-    //private float leftTriggerPressed, rightTriggerPressed;
-    //public float size;
-    public GameObject handle;
-    public TapManager tapManager; // Reference to the TapManager script.
-    public ProximityHapticFeedback proximityHapticFeedback;
-    public Material barMat;
-    public Color barColor;
-    private Image image;
+//public class BreathCounter : UIBase
+//{
+//    [Header("breath counter")]
+//    public Scrollbar progressBar;
+//    //private float leftTriggerPressed, rightTriggerPressed;
+//    //public float size;
+//    public GameObject handle;
+//    public TapManager tapManager; // Reference to the TapManager script.
+//    public ProximityHapticFeedback proximityHapticFeedback;
+//    public Material barMat;
+//    public Color barColor;
+//    private Image image;
 
 
-    public LayerMask obstructionLayer;
-    public GameObject visualizer;
-    [Range(0,1)]
-    [SerializeField] private float BreathIncreaseRatePerBar; 
-    //increase rate make sure to follow the time.deltatime
-    private float increaseRate { get {
-            //one bar is about 0.25 fill
-            //one charge is about 0.25 = 1second
-            return (0.35f / BreathIncreaseRatePerBar) * Time.deltaTime;
-        }}
-    //if you want to reduce the anxiety by 25% then the reduction to be 1 - 25% = 0.75
-    //since we are just doing an overall reduction
-    [Range(0, 1)]
-    [SerializeField] private float anxietyReductionForDeep = 0.75f;
-    [Range(0, 1)]
-    [SerializeField] private float anxietyReductionForMedium = 0.85f;
-    [Range(0, 1)]
-    [SerializeField] private float anxietyReductionForShallow = 0.92f;
-    [Range(0, 1)]
-    [SerializeField] private float anxietyReductionForlittle = 0.96f;
-    //will tell if the breath counter can run or not
-    private bool canBreath = false;
-    private bool hasFinishedBreathing = false;
-    private BreathingState currentState;
-    private BreathingState prevState;
-    EventManager<PlayerEvents> em = EventSystem.player;
+//    public LayerMask obstructionLayer;
+//    public GameObject visualizer;
+//    [Range(0,1)]
+//    [SerializeField] private float BreathIncreaseRatePerBar; 
+//    //increase rate make sure to follow the time.deltatime
+//    private float increaseRate { get {
+//            //one bar is about 0.25 fill
+//            //one charge is about 0.25 = 1second
+//            return (0.35f / BreathIncreaseRatePerBar) * Time.deltaTime;
+//        }}
+//    //if you want to reduce the anxiety by 25% then the reduction to be 1 - 25% = 0.75
+//    //since we are just doing an overall reduction
+//    [Range(0, 1)]
+//    [SerializeField] private float anxietyReductionForDeep = 0.75f;
+//    [Range(0, 1)]
+//    [SerializeField] private float anxietyReductionForMedium = 0.85f;
+//    [Range(0, 1)]
+//    [SerializeField] private float anxietyReductionForShallow = 0.92f;
+//    [Range(0, 1)]
+//    [SerializeField] private float anxietyReductionForlittle = 0.96f;
+//    //will tell if the breath counter can run or not
+//    private bool canBreath = false;
+//    private bool hasFinishedBreathing = false;
+//    private BreathingState currentState;
+//    private BreathingState prevState;
+//    EventManager<PlayerEvents> em = EventSystem.player;
 
-    void Start()
-    {
-        SetScrollbarSize(0f);
-        //barMat.color = barColor;
-        image = handle.GetComponent<Image>();
+//    void Start()
+//    {
+//        SetScrollbarSize(0f);
+//        //barMat.color = barColor;
+//        image = handle.GetComponent<Image>();
 
         
-    }
+//    }
 
-    protected override void Update()
-    {
-        base.Update();
-        if (progressBar.size != 0)
-        {
-            visualizer.SetActive(true);
-            ChangeBarColor();
-        }
-        else
-        {
-            //once the visualizer is set to zero decide on the effect
-            visualizer.SetActive(false);
-            DecideEffect();
-        }
+//    protected override void Update()
+//    {
+//        base.Update();
+//        if (progressBar.size != 0)
+//        {
+//            visualizer.SetActive(true);
+//            ChangeBarColor();
+//        }
+//        else
+//        {
+//            //once the visualizer is set to zero decide on the effect
+//            visualizer.SetActive(false);
+//            DecideEffect();
+//        }
 
-        Breath();
-    }
+//        Breath();
+//    }
 
-    public void SetScrollbarSize(float size)
-    {
-        // Ensure the size stays within the range [0, 1]
-        size = Mathf.Clamp01(size);
+//    public void SetScrollbarSize(float size)
+//    {
+//        // Ensure the size stays within the range [0, 1]
+//        size = Mathf.Clamp01(size);
 
-        // Set the size of the scrollbar handle
-        progressBar.size = size;
-    }
+//        // Set the size of the scrollbar handle
+//        progressBar.size = size;
+//    }
 
-    public void Breath()
-    {
-        if (canBreath)
-        {
-            hasFinishedBreathing = false;
-            SetScrollbarSize(progressBar.size + increaseRate);
-        }
-        else 
-        {
-            if (!hasFinishedBreathing)
-            {
-                hasFinishedBreathing = true;
-                ///record down the previous breath state
-                prevState = currentState;
-            }
-            SetScrollbarSize(progressBar.size - increaseRate);
-        }
-        DecideBreathingState();
+//    public void Breath()
+//    {
+//        if (canBreath)
+//        {
+//            hasFinishedBreathing = false;
+//            SetScrollbarSize(progressBar.size + increaseRate);
+//        }
+//        else 
+//        {
+//            if (!hasFinishedBreathing)
+//            {
+//                hasFinishedBreathing = true;
+//                ///record down the previous breath state
+//                prevState = currentState;
+//            }
+//            SetScrollbarSize(progressBar.size - increaseRate);
+//        }
+//        DecideBreathingState();
         
-    }
+//    }
 
-    private void DecideBreathingState()
-    {
-        if(progressBar.size > 0.75)
-        {
-            currentState = BreathingState.Deep;
-        }
-        else if(progressBar.size > 0.5)
-        {
-             currentState= BreathingState.Medium;
-        }
-        else if(progressBar.size > 0.25)
-        {
-            currentState = BreathingState.Shallow;
-        }
-        else if(progressBar.size > 0.1)
-        {
-            currentState = BreathingState.litte;
-        }
-    }
+//    private void DecideBreathingState()
+//    {
+//        if(progressBar.size > 0.75)
+//        {
+//            currentState = BreathingState.Deep;
+//        }
+//        else if(progressBar.size > 0.5)
+//        {
+//             currentState= BreathingState.Medium;
+//        }
+//        else if(progressBar.size > 0.25)
+//        {
+//            currentState = BreathingState.Shallow;
+//        }
+//        else if(progressBar.size > 0.1)
+//        {
+//            currentState = BreathingState.litte;
+//        }
+//    }
 
-    //probably have to change this
-    public void ChangeBarColor()
-    {
-        switch (currentState)
-        {
-            case BreathingState.Deep:
-                image.color = new Color32(0, 255, 255, 255);
-                break;
-            case BreathingState.Medium:
-                image.color = new Color32(0, 255, 0, 255);
-                break;
-            case BreathingState.Shallow:
-                image.color = new Color32(255, 165, 0, 255);
-                break;
-            case BreathingState.None:
-                image.color = new Color32(255, 0, 0, 255);
-                break;
-        }
-    }
+//    //probably have to change this
+//    public void ChangeBarColor()
+//    {
+//        switch (currentState)
+//        {
+//            case BreathingState.Deep:
+//                image.color = new Color32(0, 255, 255, 255);
+//                break;
+//            case BreathingState.Medium:
+//                image.color = new Color32(0, 255, 0, 255);
+//                break;
+//            case BreathingState.Shallow:
+//                image.color = new Color32(255, 165, 0, 255);
+//                break;
+//            case BreathingState.None:
+//                image.color = new Color32(255, 0, 0, 255);
+//                break;
+//        }
+//    }
 
-    public void StartBreathCounter()
-    {
-        canBreath = true;
-    }
+//    public void StartBreathCounter()
+//    {
+//        canBreath = true;
+//    }
 
-    public void DecideEffect()
-    {
-        switch (prevState)
-        {
-            case BreathingState.litte:
-                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForlittle;
-                Breath(anxietyReductionForlittle);
-                break;
-            case BreathingState.Shallow:
-                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForShallow;
-                Breath(anxietyReductionForShallow);
-                break;
-            case BreathingState.Medium:
-                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForMedium;
-                Breath(anxietyReductionForMedium);
-                break;
-            case BreathingState.Deep:
-                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForDeep;
-                Breath(anxietyReductionForDeep);
-                break;
-        }
-        //reset the state
-        prevState = BreathingState.None;
+//    public void DecideEffect()
+//    {
+//        switch (prevState)
+//        {
+//            case BreathingState.litte:
+//                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForlittle;
+//                Breath(anxietyReductionForlittle);
+//                break;
+//            case BreathingState.Shallow:
+//                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForShallow;
+//                Breath(anxietyReductionForShallow);
+//                break;
+//            case BreathingState.Medium:
+//                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForMedium;
+//                Breath(anxietyReductionForMedium);
+//                break;
+//            case BreathingState.Deep:
+//                proximityHapticFeedback.timeWithinMaxDistance *= anxietyReductionForDeep;
+//                Breath(anxietyReductionForDeep);
+//                break;
+//        }
+//        //reset the state
+//        prevState = BreathingState.None;
 
-        void Breath(float decrease)
-        {
-            em.TriggerEvent<float>(PlayerEvents.ANXIETY_BREATHE, decrease);
-        }
-    }
+//        void Breath(float decrease)
+//        {
+//            em.TriggerEvent<float>(PlayerEvents.ANXIETY_BREATHE, decrease);
+//        }
+//    }
 
-    public void StopBreathCounter()
-    {
-        canBreath = false;
-    }
+//    public void StopBreathCounter()
+//    {
+//        canBreath = false;
+//    }
 
-    private enum BreathingState
-    {
-        None,
-        litte,
-        Shallow,
-        Medium,
-        Deep,
-    }
-}
+//    private enum BreathingState
+//    {
+//        None,
+//        litte,
+//        Shallow,
+//        Medium,
+//        Deep,
+//    }
+//}
