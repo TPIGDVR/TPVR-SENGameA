2bba8af2656e5016a62f45a4ec1f9af1db5f4749
Thom Low <thomthomlow@gmail.com>
2024-08-01 15:06:50 +0800

    Refactored the automaton

--- Assets/Scripts/Automaton/BaseAutomatonBehaviour.cs
+++ Assets/Scripts/Automaton/BaseAutomatonBehaviour.cs
@@ -1,205 +1,226 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class BaseAutomatonBehaviour : MonoBehaviour
+namespace Automaton
 {
-    protected AutomatonStates _state;
-    protected Animator _ani;
-    protected AudioSource _audio;
-    protected NavMeshAgent _agent;
-    protected float _travelCompleteThreshold = 0.001f;
-    protected float _minWaitTime = 1;
-    protected float _maxWaitTime = 3;
-    [SerializeField]
-    AudioClip[] _footStepClips;
-    [SerializeField]
-    Waypoint[] _wayPoints;
-    int _wayPointIndex = 0;
-
-    [SerializeField]
-    bool _isStationary;
-
-    [SerializeField] Transform[] _dataInterfaceT;
-
-    [Header("Movement")]
-    [SerializeField]protected float oringinalMovement = 5f;
-    [SerializeField]protected float acceptableDegree = 15f;
-    [SerializeField]protected float rotationSpeed = 3f;
-
-    #region INITIALIZATION
-    private void Awake()
+    public class BaseAutomatonBehaviour : MonoBehaviour, IScriptLoadQueuer
     {
-        ScriptLoadSequencer.Enqueue(this, (int)LevelLoadSequence.AUTOMATONS);
-    }
+        [SerializeField] protected AutomatonStates _state;
+        protected Animator _ani;
+        protected AudioSource _audio;
+        protected NavMeshAgent _agent;
+        private float _travelCompleteThreshold = 0.001f;
+        private float _minWaitTime = 1;
+        private float _maxWaitTime = 3;
+        [SerializeField]
+        AudioClip[] _footStepClips;
+        [SerializeField]
+        Waypoint[] _wayPoints;
+        int _wayPointIndex = 0;
 
-    public void Initialize()
-    {
-        _ani = GetComponent<Animator>();
-        _agent = GetComponent<NavMeshAgent>();
-        _audio = GetComponent<AudioSource>();
-        StartCoroutine(Behaviour());
-    }
-    #endregion
+        [SerializeField]
+        bool _isStationary;
 
-    //placeholder coroutine for basic behaviours
-    protected virtual IEnumerator Behaviour()
-    {
-        while (true)
+        [SerializeField] protected Transform[] _dataInterfaceT;
+        private Vector3 targetDestination;
+        [Header("Movement")]
+        [SerializeField] private float oringinalMovement = 5f;
+        [SerializeField] private float acceptableDegree = 15f;
+        [SerializeField] private float rotationSpeed = 3f;
+
+        #region INITIALIZATION
+        private void Awake()
         {
-            EvaluateState();
-            float waitTime = Random.Range(_minWaitTime, _maxWaitTime);
+            ScriptLoadSequencer.Enqueue(this, (int)LevelLoadSequence.AUTOMATONS);
+        }
 
-            switch (_state)
+        public void Initialize()
+        {
+            _ani = GetComponent<Animator>();
+            _agent = GetComponent<NavMeshAgent>();
+            _audio = GetComponent<AudioSource>();
+            StartCoroutine(Behaviour());
+        }
+        #endregion
+
+        //placeholder coroutine for basic behaviours
+        protected virtual IEnumerator Behaviour()
+        {
+            StartBehaviour();
+            while (true)
             {
-                case AutomatonStates.IDLE:
-                    yield return new WaitForSeconds(waitTime);
-                    break;
-                case AutomatonStates.WALK:
-                    yield return WalkToWayPointCoroutine();
-                    break;
-                case AutomatonStates.SCAN:
-                    yield return WalkToTPointCoroutine();
-                    break;
-                case AutomatonStates.WALK_TO_TARGET:
-                    //TODO!!!
-                default:
-                    Debug.Log("Automaton : Unable to get a state...");
-                    break;
+                EvaluateState();
+                if (_isStationary) yield return null;
+                switch (_state)
+                {
+                    case AutomatonStates.IDLE:
+                        float waitTime = Random.Range(_minWaitTime, _maxWaitTime);
+                        yield return new WaitForSeconds(waitTime);
+                        break;
+                    case AutomatonStates.WALK:
+                        yield return WalkToWayPointCoroutine();
+                        break;
+                    case AutomatonStates.SCAN:
+                        yield return WalkToTPointCoroutine();
+                        break;
+                    case AutomatonStates.WALK_TO_TARGET:
+                        yield return MovementCoroutine(targetDestination);
+                        break;
+                    //add other behaviour
+                    default:
+                        Debug.Log("Automaton : Unable to get a state...");
+                        break;
+                }
             }
         }
-    }
 
-
-    /// <summary>
-    /// Basic walk coroutine to move the automaton to a selected waypoint
-    /// </summary>
-    /// <returns></returns>
-    protected virtual IEnumerator WalkToWayPointCoroutine()
-    {
-        _wayPointIndex++;
-        if (_wayPointIndex >= _wayPoints.Length)
-            _wayPointIndex = 0;
-
-        Vector3 pos = _wayPoints[_wayPointIndex].Position;
-        yield return new WaitForSeconds(_wayPoints[_wayPointIndex].Delay);
-        SetDestination(pos);
-
-        yield return new WaitForSeconds(0.1f);
-        yield return MovementCoroutine();
-        _ani.SetFloat("Spd", 0f);
-    }
-
-    protected virtual IEnumerator WalkToTPointCoroutine()
-    {
-        if (_dataInterfaceT.Length <= 0) yield break;
-        int index = Random.Range(0, _dataInterfaceT.Length);
-        if (_dataInterfaceT[index] != null)
-            SetDestination(_dataInterfaceT[index].position);
-
-        yield return new WaitForSeconds(0.1f);
-        yield return MovementCoroutine();
-        _ani.SetFloat("Spd", 0f);
-    }
-
-     IEnumerator MovementCoroutine()
-    {
-        Vector3 previousPosition = transform.position;
-        while (_agent.remainingDistance >= _travelCompleteThreshold)
+        /// <summary>
+        /// Basic walk coroutine to move the automaton to a selected waypoint
+        /// </summary>
+        /// <returns></returns>
+        protected virtual IEnumerator WalkToTPointCoroutine()
         {
-            //check 
-            Vector3 nxtDirection = _agent.nextPosition - previousPosition;
+            Vector3 destination = Vector3.zero;
+            if (_dataInterfaceT.Length <= 0) yield break;
+            int index = Random.Range(0, _dataInterfaceT.Length);
+            if (_dataInterfaceT[index] != null)
+                destination = _dataInterfaceT[index].position;
+            yield return MovementCoroutine(destination);
+        }
+        protected virtual IEnumerator WalkToWayPointCoroutine()
+        {
+            _wayPointIndex++;
+            if (_wayPointIndex >= _wayPoints.Length)
+                _wayPointIndex = 0;
 
-            float degreeOfRotation = Vector3.SignedAngle(transform.forward, nxtDirection, transform.up);
-            if (!(degreeOfRotation < acceptableDegree &&
-                degreeOfRotation > -acceptableDegree))
-            //if not within acceptable degree.
+            Vector3 pos = _wayPoints[_wayPointIndex].Position;
+            yield return new WaitForSeconds(_wayPoints[_wayPointIndex].Delay);
+            yield return MovementCoroutine(pos);
+        }
+        
+        #region walking
+
+        protected IEnumerator MovementCoroutine(Vector3 destination)
+        {
+            _agent.SetDestination(destination);
+            _agent.speed = 0;
+
+            yield return new WaitForSeconds(0.1f);
+
+            Vector3 previousPosition = transform.position;
+            while (_agent.remainingDistance >= _travelCompleteThreshold)
             {
-                yield return RotateKyle(degreeOfRotation, nxtDirection, transform.forward);
+                //check 
+                Vector3 nxtDirection = _agent.nextPosition - previousPosition;
+
+                float degreeOfRotation = Vector3.SignedAngle(transform.forward, nxtDirection, transform.up);
+                if (!(degreeOfRotation < acceptableDegree &&
+                    degreeOfRotation > -acceptableDegree))
+                //if not within acceptable degree.
+                {
+                    yield return RotateKyle(degreeOfRotation, nxtDirection, transform.forward);
+                }
+                else
+                {
+                    _ani.SetFloat("Spd", 0.5f);
+                    _agent.speed = oringinalMovement;
+                }
+
+                //print($"rotation;{degreeOfRotation}, forward {transform.forward}, nxtDirection {nxtDirection} \n" +
+                //    $"agent nxt Position {_agent.nextPosition}, transform position {transform.position}");
+                previousPosition = transform.position;
+                yield return null;
             }
-            else
+            _ani.SetFloat("Spd", 0f);
+        }
+
+        IEnumerator RotateKyle(float degree, Vector3 targetDirection, Vector3 currentDirection)
+        {
+            print("Rotating kyle");
+            _agent.speed = 0;
+            float strength = degree / 180;
+
+            _ani.SetFloat("RotationStrength", strength);
+            _ani.SetFloat("Spd", 0f);
+
+            _ani.SetTrigger("Rotate");
+            targetDirection.Normalize();
+            currentDirection.Normalize();
+            yield return new WaitUntil(() => _ani.GetCurrentAnimatorStateInfo(0).IsName("Rotation blend tree"));
+
+            while (_ani.GetCurrentAnimatorStateInfo(0).IsName("Rotation blend tree"))
             {
-                _ani.SetFloat("Spd", 0.5f);
-                _agent.speed = oringinalMovement;
-
+                Debug.DrawRay(transform.position, targetDirection * 50, Color.blue);
+                float normalizeTime = _ani.GetCurrentAnimatorStateInfo(0).normalizedTime;
+                transform.forward = Vector3.Slerp(currentDirection, targetDirection, normalizeTime);
+                yield return null;
             }
-            //print($"rotation;{degreeOfRotation}, forward {transform.forward}, nxtDirection {nxtDirection} \n" +
-            //    $"agent nxt Position {_agent.nextPosition}, transform position {transform.position}");
-            previousPosition = transform.position;
-            yield return null;
         }
-    }
-
-    IEnumerator RotateKyle(float degree, Vector3 targetDirection, Vector3 currentDirection)
-    {
-        _agent.speed = 0;
-        float strength = degree / 180;
-
-        _ani.SetFloat("RotationStrength", strength);
-        _ani.SetFloat("Spd", 0f);
-
-        _ani.SetTrigger("Rotate");
-        targetDirection.Normalize();
-        currentDirection.Normalize();
-        yield return new WaitUntil(() => _ani.GetCurrentAnimatorStateInfo(0).IsName("Rotation blend tree"));
-
-        while (_ani.GetCurrentAnimatorStateInfo(0).IsName("Rotation blend tree"))
+        #endregion
+        /// <summary>
+        /// Change the evaluate state for other automaton states
+        /// </summary>
+        protected virtual void EvaluateState()
         {
-            Debug.DrawRay(transform.position, targetDirection * 50, Color.blue);
-            float normalizeTime = _ani.GetCurrentAnimatorStateInfo(0).normalizedTime;
-            transform.forward = Vector3.Slerp(currentDirection, targetDirection, normalizeTime);
-            yield return null;
+            if (_isStationary)
+            {
+                _state = AutomatonStates.IDLE;
+                return;
+            }
+
+            int nxtState = (int)_state + 1;
+            if (nxtState == 3)
+            {
+                nxtState = 0;
+            }
+            _state = (AutomatonStates)nxtState;
         }
-    }
 
-    public void SetDestination(Vector3 pos)
-    {
-        _agent.SetDestination(pos);
-        _agent.speed = 0;
-
-        //_ani.SetFloat("Spd", 0.5f);
-    }
-
-    void EvaluateState()
-    {
-        //AAAAA.AAAAAA.O.MMM.MMMMMMMM.AAAAAAAAA?
-        if (_isStationary)
+        protected virtual void StartBehaviour()
         {
-            _state = AutomatonStates.IDLE;
-            return;
+            //NOOP
         }
-
-        int nxtState = (int)_state + 1;
-        if (nxtState == 3)
+        protected void ChangeState(AutomatonStates stateToChange)
         {
-            nxtState = 0;
+            _state = stateToChange;
+            StopAllCoroutines();
+            StartCoroutine(Behaviour());
         }
-        _state = (AutomatonStates)nxtState;
-    }
 
-    protected void ChangeState(AutomatonStates stateToChange)
-    {
-        _state = stateToChange;
-        StopAllCoroutines();
-        StartCoroutine(Behaviour());
-    }
+        protected enum AutomatonStates
+        {
+            IDLE,
+            WALK,
+            SCAN,
+            WALK_TO_TARGET,
+        }
 
-    protected enum AutomatonStates
-    {
-        IDLE,
-        WALK,
-        SCAN,
-        WALK_TO_TARGET,
+        [ContextMenu("Change to walk")]
+        public void ChangeToWalkToWayPoint()
+        {
+            ChangeState(AutomatonStates.WALK);
+        }
+
+        [ContextMenu("Change to Idle")]
+        public void ChangeToIdle()
+        {
+            ChangeState(AutomatonStates.IDLE);
+        }
+
+        public void ChangeToMoveTarget(Vector3 targetDestination)
+        {
+            this.targetDestination
+        }
+
+        #region misc functions
+        //called in the animator
+        void OnFootstep()
+        {
+            float vol = Random.Range(0.8f, 1f);
+            int index = Random.Range(0, _footStepClips.Length);
+            _audio.PlayOneShot(_footStepClips[index], vol);
+        }
+        #endregion
     }
-    #region misc functions
-    //called in the animator
-    void OnFootstep()
-    {
-        float vol = Random.Range(0.8f, 1f);
-        int index = Random.Range(0, _footStepClips.Length);
-        _audio.PlayOneShot(_footStepClips[index], vol);
-    }
-    #endregion
 }
