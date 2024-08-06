f0c6484790670622725a53054ed19ff88cbf0b55
Ian Wong <ianw35091@gmail.com>
2024-08-06 10:56:49 +0800

    SHTIBDIUO

--- Assets/Scripts/Player/Anxiety Scripts/PlayerAnxietyHandler.cs
+++ Assets/Scripts/Player/Anxiety Scripts/PlayerAnxietyHandler.cs
@@ -78,7 +78,6 @@ namespace Assets.Scripts.Player.Anxiety_Scripts
             if (!CanRun || isDead)
             {
                 _anxietyIncreaseScale = 0f;
-                //em_t.TriggerEvent(TutorialEvents.TUTORIAL_DEATH);
             }
 
             DetermineAnxietyLevel();
