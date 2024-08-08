e3628dd7c5db805a4bebb0e04b2d23deefa544b9
Ian Wong <ianw35091@gmail.com>
2024-08-08 16:47:58 +0800

    SKULLFAJOHFJNKFJAHL

---
+++ Assets/Scripts/HighlightControllerComponents.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightControllerComponents : MonoBehaviour, IScriptLoadQueuer
+{
+    [SerializeField]
+    Material materialNormal;
+    [SerializeField]
+    Material materialHighlight;
+
+    [SerializeField]
+    GameObject thumbstick_left;
+    [SerializeField]
+    GameObject thumbstick_right;
+    [SerializeField]
+    GameObject button_a;
+
+    EventManager<DialogEvents> em_Controller = EventSystem.dialog;
+
+    public void Initialize()
+    {
+        em_Controller.AddListener(DialogEvents.STOP_HIGHLIGHTING_ABUTTON_HIGHLIGHT_THUMBSTICKS);
+    }
+}
