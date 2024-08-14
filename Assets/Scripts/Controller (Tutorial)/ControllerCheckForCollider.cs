550dece6ace1030ff3650c817195760b74237745
Ian Wong <ianw35091@gmail.com>
2024-08-14 12:46:48 +0800

    aaa

--- Assets/Scripts/Controller (Tutorial)/ControllerCheckForCollider.cs
+++ Assets/Scripts/Controller (Tutorial)/ControllerCheckForCollider.cs
@@ -6,31 +6,33 @@ using UnityEngine;
 public class ControllerCheckForCollider : MonoBehaviour
 {
     [SerializeField]
+    SphereCollider sphereCollider;
+    [SerializeField]
     Transform parentControllerComponent;
     [SerializeField]
-    float detectionRadius = 0.05f;
+    float detectionRadius = 0.001f;
 
     Vector3 shiftOffset;
 
     private void Start()
     {
-        shiftOffset = transform.position - parentControllerComponent.position;
+        shiftOffset = transform.position - sphereCollider.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = parentControllerComponent.position + shiftOffset;
+        transform.position = sphereCollider.transform.position + shiftOffset;
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
 
         foreach (Collider collider in hitColliders)
         {
-            if (collider.transform == parentControllerComponent)
+            if (collider == sphereCollider)
             {
-                Vector3 dir = (transform.position - parentControllerComponent.position).normalized;
-                transform.position = parentControllerComponent.position + dir * detectionRadius;
+                Vector3 dir = (transform.position - sphereCollider.transform.position).normalized;
+                transform.position = sphereCollider.transform.position + dir * detectionRadius;
 
-                shiftOffset = transform.position - parentControllerComponent.position;
+                shiftOffset = transform.position - sphereCollider.transform.position;
                 break;
             }
         }
