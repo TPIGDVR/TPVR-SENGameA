b559bb799223df0c4fac000668a11492b1ca08ce
Ian Wong <ianw35091@gmail.com>
2024-08-14 12:38:23 +0800

    edada

--- Assets/Scripts/Controller (Tutorial)/ControllerCheckForCollider.cs
+++ Assets/Scripts/Controller (Tutorial)/ControllerCheckForCollider.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ControllerCheckForCollider : MonoBehaviour
 {
     [SerializeField]
-    SphereCollider sphereCollider;
-    [SerializeField]
     Transform parentControllerComponent;
+    [SerializeField]
+    float detectionRadius = 0.05f;
 
     Vector3 shiftOffset;
 
@@ -20,11 +21,18 @@ public class ControllerCheckForCollider : MonoBehaviour
     void Update()
     {
         transform.position = parentControllerComponent.position + shiftOffset;
-        Vector3 closestPoint = sphereCollider.ClosestPoint(transform.position);
-        if (Vector3.Distance(transform.position, closestPoint) < 0.001f)
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        foreach (Collider collider in hitColliders)
         {
-            transform.position = closestPoint;
+            if (collider.transform == parentControllerComponent)
+            {
+                Vector3 dir = (transform.position - parentControllerComponent.position).normalized;
+                transform.position = parentControllerComponent.position + dir * detectionRadius;
+
+                shiftOffset = transform.position - parentControllerComponent.position;
+                break;
+            }
         }
-        shiftOffset = transform.position - parentControllerComponent.position;
     }
 }
