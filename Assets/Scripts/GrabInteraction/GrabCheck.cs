4aac91f64e7936963cc44a547b9767e7f933224c
Thom Low <thomthomlow@gmail.com>
2024-09-06 13:52:15 +0800

    safe

--- Assets/Scripts/GrabInteraction/GrabCheck.cs
+++ Assets/Scripts/GrabInteraction/GrabCheck.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class GrabCheck : MonoBehaviour
 {
-    public GameObject go;
+    //public GameObject go;
     [SerializeField]
     bool IsRight;
-
+    [SerializeField] float searchRadius = 0.05f;
+    //[SerializeField] int mask = LayerMask.GetMask("Interactables");
+    [SerializeField] LayerMask interactableMask;
     private void Awake()
     {
         if (IsRight)
@@ -18,33 +21,40 @@ public class GrabCheck : MonoBehaviour
         {
             EventSystem.player.AddListener(PlayerEvents.GRAB_DOWN_LEFT, Retrieve);
         }
-        
-        
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        go = other.gameObject;
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    go = other.gameObject;
+    //}
 
-    private void OnTriggerStay(Collider other)
-    {
-        go = other.gameObject;
-    }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    go = other.gameObject;
+    //}
 
 
-    private void OnTriggerExit(Collider other)
-    {
-        go = null;
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    go = null;
+    //}
 
     private GameObject Retrieve()
     {
-        return go;
+        var collider = Physics.OverlapSphere(transform.position, searchRadius, interactableMask);
+        if (collider == null) return null;
+        //get the first one.
+        else return collider[0].gameObject;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
     }
 
     //iouytr
-   
+
 }
 
 
