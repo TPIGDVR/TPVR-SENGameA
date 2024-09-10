4e900211c9d1a1a13cf5604a5824e2750ce14d22
Ryan Yak <ryanyakkie@gmail.com>
2024-09-10 12:22:57 +0800

    Fix death

--- Assets/Scripts/UI Scripts/ScannerUI.cs
+++ Assets/Scripts/UI Scripts/ScannerUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using SoundRelated;
 using UnityEngine;
 using UnityEngine.Events;
@@ -26,6 +27,9 @@ public class ScannerUI : MonoBehaviour , IScriptLoadQueuer
     SoundManager audioPlayer;
     AudioSource globalAudioSource = null;
 
+    //due to some weird bug in the editor
+    bool pauseUpdateLoop = false;
+
     private void Awake()
     {
         ScriptLoadSequencer.Enqueue(this, (int)LevelLoadSequence.LEVEL);
@@ -40,7 +44,7 @@ public class ScannerUI : MonoBehaviour , IScriptLoadQueuer
 
     void Update()
     {
-        if (scanCompleted || (progress <= 0 && !scanning)) return;
+        if (scanCompleted || (progress <= 0 && !scanning) || pauseUpdateLoop) return;
 
         if (scanning && !scanCompleted)
         {
@@ -79,7 +83,9 @@ public class ScannerUI : MonoBehaviour , IScriptLoadQueuer
 
     void Complete()
     {
+        print("Calling Complete scanner");
         scanCompleted = true;
+        pauseUpdateLoop = true;
         onCompleteScan?.Invoke();
         audioPlayer.RetrieveAudioSource(globalAudioSource);
         audioPlayer.PlayAudioOneShot(SoundRelated.SFXClip.SCAN_SUCCESS, transform.position);
@@ -87,9 +93,18 @@ public class ScannerUI : MonoBehaviour , IScriptLoadQueuer
 
      public void ResetScanner()
     {
-        scanCompleted = false;
+        print("reset scanner");
         progress = 0;
+        scanCompleted = false;
+        scanning = false;
         UpdateProgressUI();
+        StartCoroutine(WaitToResetScanner());
+    }
+
+    IEnumerator WaitToResetScanner()
+    {
+        yield return null;
+        pauseUpdateLoop = false;
     }
 
     #region Simple Interactor
