79c169e988dbe78722f0fe102fe748fcd655236b
ThomTurtle <117958209+ThomJustAway@users.noreply.github.com>
2025-03-03 12:59:43 +0800

    safe

--- Assets/Scripts/GrabInteraction/Interable Object/HeadPhonesInterable.cs
+++ Assets/Scripts/GrabInteraction/Interable Object/HeadPhonesInterable.cs
@@ -7,13 +7,11 @@ public class HeadPhonesInterable : Interactable
     protected override void OnEquip()
     {
 
-        base.OnEquip();
         GameData.player.IsWearingHeadphones = true;
     }
 
     protected override void OnUnEquip()
     {
-        base.Unequip();
         GameData.player.IsWearingHeadphones = false;
     }
 }
